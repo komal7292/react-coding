698a8f0961a5f0f3c42f7c7d190b0010421ef5fb
komal <kg681278@gmail.com>
2020-12-24 16:32:41 +0530

    adding some new codes

--- classreact/src/index.js
+++ classreact/src/index.js
@@ -2,13 +2,29 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends Component {
-  render(){
+  constructor(props){
+    super(props)
+
+    this.state = {lat: null, long: null}
     window.navigator.geolocation.getCurrentPosition(
-      position => console.log(position),
+      position => {
+        // We called setState!!!
+        this.setState({ lat: position.coords.latitude})
+        this.setState({ long: position.coords.longitude})
+        console.log(position)
+      },
       err => console.log(err)
     )
+  }
 
-     return <div>Latitude:</div>
+  render(){
+    return(
+      <div>
+        latitude: {this.state.lat}
+        longitude: {this.state.long}
+      </div>
+    ) 
   }
 }
+
 ReactDOM.render(<App />,document.querySelector("#root"));
